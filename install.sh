4afbe4d70d79f10aa6a56c9ac02fa9bbc6b5f180
ross-p-smith <rosmith@microsoft.com>
2022-10-10 05:59:54 +0100

    Add PowerLevel10k

--- install.sh
+++ install.sh
@@ -10,3 +10,22 @@ BASE_DIR=$(dirname "$0")
 BASE_DIR=$(cd $BASE_DIR; pwd)
 
 echo "dotfiles/install.sh - starting... (DEV_CONTAINER=$DEV_CONTAINER)"
+
+# Install a custom theme.
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+if grep -q DOTFILES_FOLDER ~/.bashrc; then
+    echo "dotfiles loader already in .bashrc - skipping"
+else
+    echo "Adding dotfiles loader to .bashrc..."
+    echo -e "# DOTFILES_START" >> ~/.bashrc
+    echo -e "DOTFILES_FOLDER=\"$BASE_DIR\"" >> ~/.bashrc
+    if [[ -n $DEV_CONTAINER ]]; then
+        echo -e "DEV_CONTAINER=1" >> ~/.bashrc
+    fi
+    echo -e "source \"$BASE_DIR/load.sh\"" >> ~/.bashrc
+    echo -e "# DOTFILES_END\n" >> ~/.bashrc
+fi
+
+echo "dotfiles/install.sh - done."
---
+++ load.sh
@@ -0,0 +1,8 @@
+#
+# This script is intended to be sourced from .bashrc
+# It is added to .bashrc by install.sh
+#
+
+if [[ -z $DEV_CONTAINER ]];then
+    TZ='Europe/London'; export TZ
+fi
